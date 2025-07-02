64d8e8a74aa88af7bc8655c81a63ca18d63863ea
Wioletta Koperska <koperskawioletta@gmail.com>
2025-07-02 15:10:05 +0100

    style: add color palette to theme for consistent styling

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,15 @@
 export const theme = {
   color: {
     white: "#FFFFFF",
+    pattensblue: "#D6E4FF",
     whisper: "#F5F5FA",
     mystic: "#E4E6F0",
+    silver: "#C4C4C4",
+    candlelight: "#FCD420",
+    scienceblue: "#0044CC",
+    lynch: "#697E9A",
+    stormgrey: "#74788B",
+    waterloo: "#7E839A",
     black: "#000000",
   },
 };
