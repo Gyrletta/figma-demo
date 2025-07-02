30078f6492e58f383bcfa91d2015c66010165de6
Wioletta Koperska <koperskawioletta@gmail.com>
2025-07-02 15:12:16 +0100

    feat: implement full Navigation component with routing and styled layout

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Camera } from "./../../images/Camera.svg";
+
+import { StyledNav, Logo, LogoText, NavMenu, List, Item } from "./styled";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <StyledNav>
+      <Logo>
+        <NavLink to="/movielist">
+          <Camera />
+          <LogoText>Movies Browser</LogoText>
+        </NavLink>
+      </Logo>
+
+      <NavMenu>
+        <List>
+          <Item>
+            <NavLink to="/movielist">Movies</NavLink>
+          </Item>
+          <Item>
+            <NavLink to="/peoplelist">People</NavLink>
+          </Item>
+        </List>
+      </NavMenu>
+    </StyledNav>
+  );
 };
 export default Navigation;
